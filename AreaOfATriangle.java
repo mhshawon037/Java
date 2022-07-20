17ba027a3af554649ed28baa437f56df8f4e25a3
Mehedi Hassan Shawon <109661640+mhshawon037@users.noreply.github.com>
2022-07-20 17:08:50 +0600

    Add files via upload

---
+++ AreaOfATriangle.java
@@ -0,0 +1,14 @@
+
+public class AreaOfATriangle {
+
+	public static void main(String[] args) {
+		int base, height;
+		double area;
+		base = 5;
+		height = 7;
+		area = (base * height) /2.0;
+		System.out.print("Area of a Triangle is : " + area);
+
+	}
+
+}
