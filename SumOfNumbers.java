473d9bcfdfb59a9f8e191b457beb85482fa087f8
Mehedi Hassan Shawon <109661640+mhshawon037@users.noreply.github.com>
2022-07-20 16:45:25 +0600

    Add files via upload

---
+++ SumOfNumbers.java
@@ -0,0 +1,18 @@
+
+public class SumOfNumbers {
+
+	public static void main(String[] args) {
+		int num1,num2,num3,num4;
+		double num5,sum;
+
+		num1 =10;
+		num2 = 20;
+		num3 =30;
+		num4 =40;
+		num5=5.7;
+
+		sum = num1+num2+num3+num4+num5;
+		System.out.print("Summation = "+sum);
+	}
+
+}
